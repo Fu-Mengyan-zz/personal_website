f863e40963c320074782174ae05b329d99fac241
mengyan-FU <mfu003@e.ntu.edu.sg>
2019-04-12 23:50:56 +0800

    Updates

---
+++ precache-manifest.7ca86731e5aca44236f176ac1947dd66.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "86916fdc7c0107af996c",
+    "url": "/Fu-Mengyan/resume_website/static/css/main.20535796.chunk.css"
+  },
+  {
+    "revision": "86916fdc7c0107af996c",
+    "url": "/Fu-Mengyan/resume_website/static/js/main.16488aa6.chunk.js"
+  },
+  {
+    "revision": "c0bb25a47d1173297a83",
+    "url": "/Fu-Mengyan/resume_website/static/js/runtime~main.335179ee.js"
+  },
+  {
+    "revision": "c3a692407263cbfc0249",
+    "url": "/Fu-Mengyan/resume_website/static/css/2.a8b3dfa6.chunk.css"
+  },
+  {
+    "revision": "c3a692407263cbfc0249",
+    "url": "/Fu-Mengyan/resume_website/static/js/2.c690594f.chunk.js"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/Fu-Mengyan/resume_website/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/Fu-Mengyan/resume_website/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/Fu-Mengyan/resume_website/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/Fu-Mengyan/resume_website/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/Fu-Mengyan/resume_website/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/Fu-Mengyan/resume_website/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/Fu-Mengyan/resume_website/static/media/square5.ff004a0e.png"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/Fu-Mengyan/resume_website/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/Fu-Mengyan/resume_website/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/Fu-Mengyan/resume_website/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "e37326c7e0fd9826c1a35b231340f58b",
+    "url": "/Fu-Mengyan/resume_website/static/media/path4.e37326c7.png"
+  },
+  {
+    "revision": "dddd56e9d5e84fb064ee47a76a2d2821",
+    "url": "/Fu-Mengyan/resume_website/static/media/fu_mengyan_profile.dddd56e9.jpg"
+  },
+  {
+    "revision": "bc9d77f29f49bbc92a6754593144425c",
+    "url": "/Fu-Mengyan/resume_website/index.html"
+  }
+];
